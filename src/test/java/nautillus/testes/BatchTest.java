18f02c434fa4275ae506f777fe402643c6bfec06
Kühl <felipekuhl321@gmail.com>
2026-05-15 15:15:27 -0300

    add: add batch test

--- src/test/java/nautillus/testes/BatchTest.java
+++ src/test/java/nautillus/testes/BatchTest.java
@@ -1,4 +1,35 @@
 package nautillus.testes;
 
+import nautillus.enums.AdministrationRoute;
+import nautillus.enums.Label;
+import nautillus.enums.PharmForm;
+import nautillus.enums.RemedyStatus;
+import nautillus.models.Batch;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BatchTest {
+
+    @Test
+    void batchValid() {
+        Batch batch = new Batch(
+                "Dipirona Monoidratada",
+                Label.RED,
+                PharmForm.TABLET,
+                AdministrationRoute.PO,
+                "1234567891234",
+                true,
+                RemedyStatus.ACTIVE,
+                12,
+                2.99f,
+                "01/03/2026",
+                "01/05/2026",
+                1,
+                "12/04/2026"
+                );
+
+        // Lower case test
+        assertEquals("Dipirona Monoidratada", batch.getName());
+    }
 }
+
