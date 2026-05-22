9c540a08bac3659fb8e5ab91561beeecef63c2e4
Kühl <felipekuhl321@gmail.com>
2026-05-22 12:01:57 -0300

    add: springboot controller and models

---
+++ src/main/java/nautillus/Main.java
@@ -0,0 +1,12 @@
+package nautillus;
+
+import nautillus.controllers.NautillusController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(NautillusController.class, args);
+    }
+}
